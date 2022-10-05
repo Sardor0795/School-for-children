38bb0adf87ef3453f26de7e81ab45b02609f8d9d
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-10-05 15:11:12 +0500

    Add site loader

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ const elMobile = document.querySelector(".mobile");
 const elBurgerBtn = document.querySelector(".header__burger");
 const elClose = document.querySelector(".close");
 
+const elLoader = document.querySelector(".loader-wrapper");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    elLoader.classList.add("close");
+  }, 1000);
+});
+
 elBurgerBtn.addEventListener("click", function () {
   elMobile.classList.add("active");
   document.body.classList.add("noScroll");
